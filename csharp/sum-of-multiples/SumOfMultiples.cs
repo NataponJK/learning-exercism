1643aebc78d7c6eccf652393cc44675fafa16204
Natapon Jaikam <nataponj@gmail.com>
2025-07-24 19:33:17 +0700

    finished SumOfMultiples-LINQ

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,7 @@
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    => Enumerable.Range(1, max - 1)
+        .Where(i => multiples.Any(m => m != 0 && i % m == 0))
+        .Sum();
+}
