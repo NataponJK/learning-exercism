15284ea05ed83ab1a270da51e05aafb3370cba3b
Natapon Jaikam <nataponj@gmail.com>
2025-10-19 22:11:39 +0700

    finished Connect

---
+++ csharp/connect/Connect.cs
@@ -0,0 +1,63 @@
+public enum ConnectWinner
+{
+    White,
+    Black,
+    None
+}
+
+public class Connect
+{
+    private readonly char[][] board;
+    private static char[] CreateRow(string line)
+    => line.Replace(" ", "").ToCharArray();
+
+    public Connect(string[] input)
+    => this.board = input.Select(CreateRow).ToArray();
+
+    public ConnectWinner Result()
+    {
+        var visited = new HashSet<(int, int)>();
+        var s = new Stack<(int, int, char)>();
+        //Note: X = Black, O = White
+        for (int y = 0; y < board.Length; y++)
+        {
+            s.Push((0, y, 'X'));
+        }
+        for (int x = 0; x < board[0].Length; x++)
+        {
+            s.Push((x, 0, 'O'));
+        }
+        while (s.Any())
+        {
+            var (x, y, token) = s.Pop();
+            try
+            {
+                if (board[y][x] != token || visited.Contains((x, y))) continue;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                continue;
+            }
+            switch (token)
+            {
+                case 'X':
+                    if (x == board[y].Length - 1) return ConnectWinner.Black;
+                    break;
+                case 'O':
+                    if (y == board.Length - 1) return ConnectWinner.White;
+                    break;
+            }
+            visited.Add((x, y));
+            for (int m = -1; m < 2; m++)
+            {
+                for (int n = -1; n < 2; n++)
+                {
+                    if (m == n) continue;
+                    s.Push((x + m, y + n, token));
+                }
+            }
+        }
+        return ConnectWinner.None;
+    }
+
+}
