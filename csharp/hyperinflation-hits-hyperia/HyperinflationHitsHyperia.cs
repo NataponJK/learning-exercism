58a2871e080d5be17ee244b1c2be43b3a11b038d
Natapon Jaikam <nataponj@gmail.com>
2025-12-16 20:00:03 +0700

    finished HyperinflationHitsHyperia

---
+++ csharp/hyperinflation-hits-hyperia/HyperinflationHitsHyperia.cs
@@ -0,0 +1,32 @@
+public static class CentralBank
+{
+    public static string DisplayDenomination(long @base, long multiplier)
+    {
+        try
+        {
+            return $"{checked(@base * multiplier)}";
+        }
+        catch(OverflowException)
+        {
+            return "*** Too Big ***";
+        }
+    }
+
+    public static string DisplayGDP(float @base, float multiplier)
+    {
+        float total = @base * multiplier;
+        return float.IsInfinity(total) ? "*** Too Big ***" : total.ToString();
+    }
+
+    public static string DisplayChiefEconomistSalary(decimal salaryBase, decimal multiplier)
+    {
+        try
+        {
+            return $"{checked(salaryBase * multiplier)}";
+        }
+        catch (OverflowException)
+        {
+            return "*** Much Too Big ***";
+        }
+    }
+}
