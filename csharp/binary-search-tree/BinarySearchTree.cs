2bf6bfddfa44a0662e59641d3673152cd32732aa
Natapon Jaikam <nataponj@gmail.com>
2026-01-07 19:35:36 +0700

    finished BinarySearchTree

---
+++ csharp/binary-search-tree/BinarySearchTree.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+
+public class BinarySearchTree : IEnumerable<int>
+{
+    public BinarySearchTree(int value)
+    => Value = value;
+
+    public BinarySearchTree(IEnumerable<int> values)
+    {
+        Value = values.First();
+        foreach(var value in values.Skip(1)) Add(value);
+    }
+
+    public int Value { get; private set; }
+    public BinarySearchTree? Left { get; private set;}
+    public BinarySearchTree? Right { get; private set;}
+
+    public BinarySearchTree Add(int value)
+    {
+        if (value <= Value)
+            Left = Left?.Add(value) ?? new BinarySearchTree(value);
+        else
+            Right = Right?.Add(value) ?? new BinarySearchTree(value);
+        return this;
+    }
+    public IEnumerator<int> GetEnumerator()
+    {
+        if (Left != null)
+            foreach (var leftValue in Left) yield return leftValue;
+        yield return Value;
+        if (Right != null)
+            foreach (var rightValue in Right) yield return rightValue;
+    }
+    IEnumerator IEnumerable.GetEnumerator()
+    => GetEnumerator();
+}
