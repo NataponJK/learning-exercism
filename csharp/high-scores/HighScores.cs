643889dda77fda6ed30bf2865b6ad40517fb2d38
Natapon Jaikam <nataponj@gmail.com>
2026-01-13 18:13:40 +0700

    finished HighScores

---
+++ csharp/high-scores/HighScores.cs
@@ -0,0 +1,16 @@
+public class HighScores
+{
+    private readonly List<int> _scores;
+    public HighScores(List<int> list)
+    {
+        _scores = list;
+    }
+
+    public List<int> Scores() => _scores;
+
+    public int Latest() => _scores.Last();
+
+    public int PersonalBest() => _scores.Max();
+
+    public List<int> PersonalTopThree() => _scores.OrderByDescending(score => score).Take(3).ToList();
+}
