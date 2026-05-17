d6cd70bb5e77a41c73ec3d04303ed7e5d83ad72d
Natapon Jaikam <nataponj@gmail.com>
2026-05-17 19:01:13 +0700

    finished javascript roman-numerals

---
+++ javascript/roman-numerals/roman-numerals.js
@@ -0,0 +1,20 @@
+//
+// This is only a SKELETON file for the 'Roman Numerals' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+const ROMAN_MAP = [
+  [1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'],
+  [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'],
+  [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']
+]
+
+export const toRoman = (number) => {
+  let result = ''
+  for (const [value, symbol] of ROMAN_MAP){
+    while (number >= value){
+      result += symbol;
+      number -= value;
+    }
+  }
+  return result;
+};
