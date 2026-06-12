66c4b2e80815b97c7681bead1dc46774a2092ff2
Natapon Jaikam <nataponj@gmail.com>
2026-06-12 20:23:27 +0700

    finished typescript ocr-numbers

---
+++ typescript/ocr-numbers/ocr-numbers.ts
@@ -0,0 +1,40 @@
+const OCR_MAP: Record<string, string> = {
+  ' _ | ||_|   ': '0',
+  '     |  |   ': '1',
+  ' _  _||_    ': '2',
+  ' _  _| _|   ': '3',
+  '   |_|  |   ': '4',
+  ' _ |_  _|   ': '5',
+  ' _ |_ |_|   ': '6',
+  ' _   |  |   ': '7',
+  ' _ |_||_|   ': '8',
+  ' _ |_| _|   ': '9',
+}
+
+export function convert(input: string): string {
+  const lines: string[] = input.split('\n');
+
+  if (lines.length % 4 !== 0) {
+    throw new Error('Number of lines must be a multiple of four.');
+  }
+  if (lines.some((line: string) => line.length % 3 !== 0)) {
+    throw new Error('Number of columns must be a multiple of three.');
+  }
+
+  const outputRows: string[] = [];
+  for (let r = 0; r < lines.length; r += 4) {
+    let rowText = '';
+    const totalDigitsInRow = lines[r].length / 3;
+    for (let d = 0; d < totalDigitsInRow; d++) {
+      const colStart = d * 3;
+      const colEnd = colStart + 3;
+      const cellKey: string = lines[r].slice(colStart, colEnd) 
+                            + lines[r + 1].slice(colStart, colEnd) 
+                            + lines[r + 2].slice(colStart, colEnd) 
+                            + lines[r + 3].slice(colStart, colEnd);
+      rowText += OCR_MAP[cellKey] ?? '?';
+    }
+    outputRows.push(rowText);
+  }
+  return outputRows.join(',');
+}
